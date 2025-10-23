6d5aec2236b799c598687fe29b7abea99007fee7
arielgamal <arielgamal@hotmail.com>
2025-10-23 11:12:02 -0300

    fix rotes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,7 @@
 import express from 'express'
-import { createUser, listUsers } from '../controllers/userController.js';
+import { createUser, listUsers, getUserById } from '../controllers/userController.js';
 
 const router = express.Router();
-const { createUser, listUsers } = require('../controllers/userController');
 
 router.get('/', (req, res) => {
     res.send('LOVE PET');
